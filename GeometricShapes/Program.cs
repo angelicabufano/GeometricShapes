c90483643c1ed98612fe44ca568eba79954507bb
Angelica Bufano <abufano@tql.com>
2024-06-17 11:44:00 -0400

    Add project files.

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Security.Cryptography;
+
+namespace GeometricShapes;
+
+internal class Program {
+    static void Main(string[] args) {
+
+        Sqr s1 = new Sqr(6);
+        Console.WriteLine($"The Perimeter for Sqr is {s1.Perimeter()} and the Area is {s1.Area()}.");
+
+        Rect r1 = new Rect(5, 10);
+        Console.WriteLine($"The Perimeter for Rect1 is {r1.Perimeter()} and the Area is {r1.Area()}.");
+
+        Quad q1 = new Quad(3, 4, 5, 6);
+        Console.WriteLine($"Perimeter for Quad1 is {q1.Perimeter()}.");
+        Quad q2 = new Quad(7, 8, 9, 10);
+        Console.WriteLine($"Perimeter for Quad2 is {q2.Perimeter()}.");
+
+    }
+}
